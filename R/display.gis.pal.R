20159b6a4b5af6d3dc560008859e7aacf4f52cd1
Sarah Goslee <sarah.goslee@gmail.com>
2017-09-22 19:19:22 -0400

    Update display.gis.pal.R

--- R/display.gis.pal.R
+++ R/display.gis.pal.R
@@ -2,9 +2,11 @@ display.gis.pal <-
 function(name, pal) {
     # plot a single GRASS palette
     if(!missing(name)) {
-        pal <- gis.pal(name, raw=TRUE)
+        pal <- gis.pal.info(name)
+    } else {
+      name <- deparse(substitute(pal))
     }
 
-    image(matrix(1:nrow(pal), ncol=1), col=pal$color, yaxt="n", xaxt="n", xlab=name)
+    image(matrix(1:nrow(pal), ncol=1), col=pal$color, yaxt="n", xaxt="n", main=name)
     axis(1, at=seq(0, 1, length=nrow(pal)), labels=paste0(pal$value, ifelse(pal$pct, "%", "")))
 }
