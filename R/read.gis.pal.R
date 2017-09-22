d4a480bc2bcfa9e2f391f61dc9ae204de3901ee8
Sarah Goslee <sarah.goslee@gmail.com>
2017-09-22 19:21:40 -0400

    Update and rename read.colors.R to read.gis.pal.R

--- R/read.gis.pal.R
+++ R/read.gis.pal.R
@@ -1,26 +1,33 @@
-read.colors <-
-function(filename) {
+read.gis.pal <-
+function(filename, text) {
 	# GRASS colortable files are messy
 	# they all have value then color
 	# value can be numeric or percent
 	# color can be name, r:g:b or r g b
 	# some are tab and some are space separated
-	colfile <- scan(filename, what=character(), sep="\n")
+	if(!missing(filename)) {
+		colfile <- scan(filename, what=character(), sep="\n")
+	} else {
+		colfile <- scan(text=text, what=character(), sep="\n")
+	}
 
     # delete comments
-    colfile <- colfile[!grepl("^#", colfile)] 
+    colfile <- colfile[!grepl("^#", colfile)]
+
+    # delete empty lines
+    colfile <- colfile[!(grepl("", colfile))]
 
     # delete leading and trailing whitespace
     colfile <- sub("^[[:space:]]+", "", colfile)
     colfile <- sub("[[:space:]]+$", "", colfile)
     # default value string is "default" - what to do with this???
-    # for now, delete it, because for the only file wereh it's present, srtm_plus
+    # for now, delete it, because for the only file where it's present, srtm_plus
     # it's white anyway, and the same as NA
-    colfile <- colfile[!grepl("^default", colfile)] 
+    colfile <- colfile[!grepl("^default", colfile)]
     # NA string is "nv"
     colfile <- colfile[!grepl("^nv", colfile)]
 
-        
+
 	value <- sapply(colfile, function(x)strsplit(x, "[[:space:]]+", fixed=FALSE)[[1]][1])
     names(value) <- NULL
 
@@ -36,11 +43,11 @@ function(filename) {
 		y <- y[-1]
 		if(length(y) == 3) {
 			y <- rgbv(as.numeric(y))
-		} else { 
+		} else {
 			y <- rgbname(y)
 		}
 		y
 	})
     names(color) <- NULL
     data.frame(value, pct, color, stringsAsFactors=FALSE)
-}
+} 
