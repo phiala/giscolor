9b3955bd6771ca531b0d4bcbcb560f47239cc937
Sarah Goslee <sarah.goslee@gmail.com>
2017-09-22 19:20:43 -0400

    Update gis.pal.info.R

--- R/gis.pal.info.R
+++ R/gis.pal.info.R
@@ -1,14 +1,21 @@
 gis.pal.info <-
-function() {
-    # similar to RColorBrewer:brewer.pal.info
-    filelist <- ls(envir=.giscolordata)
-    results <- data.frame(name=filelist, length=NA, npct=NA, min=NA, max=NA, stringsAsFactors=FALSE)
-    for(i in seq_along(filelist)) {
-        thispal <- get(filelist[i], envir=.giscolordata)
-        results$length[i] <- nrow(thispal)
-        results$npct[i] <- sum(thispal$pct)
-        results$min[i] <- min(thispal$value)
-        results$max[i] <- max(thispal$value)
+function(name) {
+    # similar to RColorBrewer::brewer.pal.info
+    if(!missing(name)) {
+      if(exists(name, envir=.GlobalEnv))
+        results <- get(name)
+      else
+        results <- get(name, envir=.giscolordata)
+    } else {
+      filelist <- ls(envir=.giscolordata)
+      results <- data.frame(name=filelist, length=NA, npct=NA, min=NA, max=NA, stringsAsFactors=FALSE)
+      for(i in seq_along(filelist)) {
+          thispal <- get(filelist[i], envir=.giscolordata)
+          results$length[i] <- nrow(thispal)
+          results$npct[i] <- sum(thispal$pct)
+          results$min[i] <- min(thispal$value)
+          results$max[i] <- max(thispal$value)
+        }
     }
     results
 }
